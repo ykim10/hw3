c2f5c21325532621e88b83549fa4ed4da6f26dd0
ykim10 <35678863+ykim10@users.noreply.github.com>
2018-02-05 20:14:31 -0800

    Create drawing-with-diameter.js

---
+++ drawing-with-diameter.js
@@ -0,0 +1,43 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+}
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  } else if (key == '1') {
+		ellipse(mouseX, mouseY, diameter+10)
+	} else if (key == '2') {
+		ellipse(mouseX, mouseY, diameter+20)
+	} else if (key == '3') {
+		ellipse(mouseX, mouseY, diameter+30)
+	}
+}
+
+
+function keyPressed() {
+  print(key);
+  if (key == 'A') {
+    fill('orange');
+  } else if (key == 'S') {
+    fill('pink');
+  } else if (key == 'D') {
+    fill('yellow'); 
+  } else if (key == 'F') {
+    fill('aqua');
+  } else if (key == 'G') {
+    fill(129, 22, 160);
+	} else if (key == 'H') {
+    fill(52, 73, 94);
+	}
+}
+// the lettering was for my own convenience
